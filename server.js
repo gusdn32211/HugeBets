2a14cfd3ea7e5148d0cac0fa336bd9584d9673eb
Alexander Lim <alexsclim@gmail.com>
2018-01-13 23:50:32 -0800

    FIX FELIX AND CLARENCE SHIT

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var http = require('http');
- 
+
 var app = express();
- 
+
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
- 
+
 app.get('/', function(req, res) {
   res.render('home', {
     title: 'Welcome'
   });
 });
- 
+
 app.get('/test',function(req, res) {
 
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 5000)
